5404d756c91cff7927540cc24938e7dd3764a821
Jan T. Sott <git@idleberg.com>
2022-09-10 01:42:01 +0200

    remove Sun and Moon icons

--- src/components/Icon/index.ts
+++ src/components/Icon/index.ts
@@ -1,5 +1,3 @@
 export { default as Chat } from './Chat.svelte';
 export { default as GitHub } from './GitHub.svelte';
 export { default as Twitter } from './Twitter.svelte';
-export { default as Moon } from './Moon.svelte';
-export { default as Sun } from './Sun.svelte';
