8dceac6f7c12f129a7b85c3b884f5aa54a4e7ccc
Jan T. Sott <git@idleberg.com>
2022-09-10 01:41:35 +0200

    add cursor

--- src/components/List/List.svelte
+++ src/components/List/List.svelte
@@ -8,7 +8,7 @@
 			<div class="inline-block md:block">{release.id.toLowerCase()}</div>
 			<div class="divider border-t-2 border-neutral-400 border-dotted mt-4 dark:border-neutral-400 hidden sm:block" />
 			<div class="inline-block md:block">
-				<a href="/downloads/{release.id}.zip" class="text-red-600 hover:bg-red-600 dark:text-rose-400 dark:hover:bg-rose-400 hover:text-white">{release.name.toLowerCase()}</a>{#if release.geoblock}<span title="Formerly a Japan exclusive pack">*</span>{/if}
+				<a href="/downloads/{release.id}.zip" class="text-red-600 hover:bg-red-600 dark:text-rose-400 dark:hover:bg-rose-400 hover:text-white">{release.name.toLowerCase()}</a>{#if release.geoblock}<span class="cursor-help" title="Formerly a Japan exclusive pack">*</span>{/if}
 				by
 				<span class="text-red-600 dark:text-rose-400">{release.byline.toLowerCase()}</span>
 			</div>
