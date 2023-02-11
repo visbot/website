31f8c6a9336648f3031cd13d560cf0d707eff8fd
Jan T. Sott <git@idleberg.com>
2023-02-12 00:26:37 +0100

    add deleted case

--- src/components/ReleaseList/ReleaseList.svelte
+++ src/components/ReleaseList/ReleaseList.svelte
@@ -12,6 +12,8 @@
 			<div class="inline-block md:block">
 				{#if release.tba}
 					<span title="To be announced">{release.name?.toLowerCase() || 't.b.a.'}</span>
+				{:else if release.deleted}
+					<del title="This item has been deleted from the catalogue" class="decoration-black text-red-600">{release.name?.toLowerCase() || 't.b.a.'}</del>
 				{:else}
 					<a href="/downloads/packs/{release.id}.exe.zip" class="text-red-600 hover:bg-red-600 dark:text-rose-400 dark:hover:bg-rose-400 hover:text-white">{release.name.toLowerCase()}</a>{#if release.geoblock?.length}<span class="cursor-help" title="Formerly a Japan exclusive pack">*</span>{/if}
 				{/if}
