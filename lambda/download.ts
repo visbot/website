9f0d0e58af9d5455dea1151f82bef15bb851b9d6
Jan T. Sott <git@idleberg.com>
2023-03-27 22:29:47 +0200

    add tracking event

--- lambda/download.ts
+++ lambda/download.ts
@@ -1,4 +1,20 @@
 exports.handler = async function (event, context) {
+	const measurement_id = process.env.GA_MEASUREMENT_ID;
+	const api_secret = process.env.GA_API_SECRET;
+
+	await fetch(`https://www.google-analytics.com/mp/collect?measurement_id=${measurement_id}&api_secret=${api_secret}`, {
+		method: 'POST',
+		body: JSON.stringify({
+			client_id: 'XXXXXXXXXX.YYYYYYYYYY',
+			events: [
+				{
+					name: 'netlify_hello_world',
+					params: {}
+				}
+			]
+		})
+	});
+
 	return {
 		statusCode: 200,
 		body: JSON.stringify({
