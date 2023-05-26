6439bb366419a30902358c55bd18268a3f8d19dd
Jan T. Sott <git@idleberg.com>
2023-05-26 22:10:16 +0200

    announce VA018

--- src/components/ReleaseList/ReleaseList.svelte
+++ src/components/ReleaseList/ReleaseList.svelte
@@ -17,8 +17,10 @@
 				{:else}
 					<a href="/download/?file={release.id}.exe.zip" class="text-red-600 hover:bg-red-600 dark:text-rose-400 dark:hover:bg-rose-400 hover:text-white">{release.name.toLowerCase()}</a>{#if release.geoblock?.length}<span class="cursor-help" title="Formerly a Japan exclusive pack">*</span>{/if}
 				{/if}
-				by
-				<span class="text-red-600 dark:text-rose-400">{release.byline.toLowerCase()}</span>
+				{#if release.byline}
+					by
+					<span class="text-red-600 dark:text-rose-400">{release.byline.toLowerCase()}</span>
+				{/if}
 			</div>
 
 			{#if release.is_new}
