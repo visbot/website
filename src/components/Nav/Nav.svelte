13f43dd29c4daf1d4516e22d676d0d2a895eeab8
Jan T. Sott <git@idleberg.com>
2023-05-28 15:32:05 +0200

    update navigation

--- src/components/Nav/Nav.svelte
+++ src/components/Nav/Nav.svelte
@@ -1,27 +1,21 @@
-<script lang="ts">
-	import { Chat, GitHub, Mastodon } from '$components/Icon';
-	import { meta as store } from '$stores/meta';
-	import metaData from '../../meta.json';
-</script>
-
 <nav class="font-semibold bg-white sm:border-2 border-black dark:border-neutral-200 dark:bg-neutral-900 dark:text-white p-4 my-0 sm:mb-4 box-shadow">
 	<h1>
 		<a href="/">VISBOT NETWORK</a>
 	</h1>
-	<ul class="float-right">
+	<ul class="float-right lowercase">
 		<li class="inline-block">
 			<a href="https://mastodon.social/@visbot" class="hover:text-red-500 dark:hover:text-rose-400" title="Follow us on Mastodon" target="_blank" rel="me noreferrer">
-				<Mastodon />
+				<span>Mastodon</span>
 			</a>
 		</li>
-		<!-- <li class="inline-block">
-			<a href="https://discord.gg/ezRVJEvu" class="hover:text-red-500 dark:hover:text-rose-400" title="Join our Discord channel" target="_blank" rel="noreferrer">
-				<Chat />
-			</a>
-		</li> -->
 		<li class="inline-block">
 			<a href="https://github.com/visbot" class="hover:text-red-500 dark:hover:text-rose-400" title="Find our projects on GitHub" target="_blank" rel="me noreferrer">
-				<GitHub />
+				<span>github</span>
+			</a>
+		</li>
+		<li class="inline-block">
+			<a href="https://visbot.net/feed.atom" class="hover:text-red-500 dark:hover:text-rose-400" title="Subscribe to our feed" target="_blank" rel="me noreferrer">
+				<span>Feed</span>
 			</a>
 		</li>
 	</ul>
