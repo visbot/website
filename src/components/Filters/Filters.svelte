734e26cf13bdf20e7788d7dc2e0531db4fa7fc27
Jan T. Sott <git@idleberg.com>
2023-05-28 15:19:29 +0200

    add safeguards

--- src/components/Filters/Filters.svelte
+++ src/components/Filters/Filters.svelte
@@ -33,8 +33,8 @@
 	};
 
 	const filterHandler = () => {
-		const search = decodeURIComponent(searchValue).toLowerCase() || '';
-		const artist = selectedArtist.toLowerCase();
+		const search = decodeURIComponent(searchValue)?.toLowerCase() || '';
+		const artist = selectedArtist?.toLowerCase();
 		const type = selectedType.toLowerCase();
 		const series = selectedSeries.toLowerCase();
 
@@ -43,7 +43,7 @@
 
 		if (search?.length) {
 			newSearchParams.set('search', search);
-			currentFilter = currentFilter.filter((item) => item.name.toLowerCase().includes(search) || item.id.toLowerCase().includes(search));
+			currentFilter = currentFilter.filter((item) => item.name?.toLowerCase().includes(search) || item.id.toLowerCase().includes(search));
 		}
 
 		if (artist?.length) {
