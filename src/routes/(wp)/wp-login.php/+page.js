a8cb9b6d222de3111e113c9be7c66495b15060b8
Jan T. Sott <git@idleberg.com>
2023-08-12 00:03:50 +0200

    add prerender=true

--- src/routes/(wp)/wp-login.php/+page.js
+++ src/routes/(wp)/wp-login.php/+page.js
@@ -1,9 +1,1 @@
-/** @type {import('./$types').PageLoad} */
-export function load({ url }) {
-	const searchParams = new URLSearchParams(url.search);
-	const isLogin = searchParams.has('action') && searchParams.get('action') === 'lostpassword' ? false : true;
-
-	return {
-		isLogin
-	};
-}
+export const prerender = true;
