fa5cf8e9acbe97cbd218d245c9f10bd8c6c2ff71
Jan T. Sott <git@idleberg.com>
2023-08-11 23:48:05 +0200

    adjust load function

--- src/routes/(wp)/wp-admin/+page.js
+++ src/routes/(wp)/wp-admin/+page.js
@@ -1,11 +1,1 @@
 export const prerender = true;
-
-/** @type {import('./$types').PageLoad} */
-export function load({ url }) {
-	const searchParams = new URLSearchParams(url.search);
-	const isLogin = searchParams.has('action') && searchParams.get('action') === 'lostpassword' ? false : true;
-
-	return {
-		isLogin
-	};
-}
