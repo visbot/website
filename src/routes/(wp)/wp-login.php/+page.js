bde0efd6696d70f317c24906b8c0db389bbea654
Jan T. Sott <git@idleberg.com>
2023-08-12 09:01:03 +0200

    add form type detection

--- src/routes/(wp)/wp-login.php/+page.js
+++ src/routes/(wp)/wp-login.php/+page.js
@@ -1,1 +1,10 @@
 export const prerender = true;
+
+export function load({ url }) {
+	const searchParams = new URLSearchParams(url.search);
+	const formType = searchParams.has('action') && searchParams.get('action') === 'lostpassword' ? 'lostpassword' : 'login';
+
+	return {
+		formType
+	};
+}
