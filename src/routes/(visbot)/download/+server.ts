ab3126e2763333f94b92718e373f7a50c0b7ff03
Jan T. Sott <git@idleberg.com>
2023-08-15 17:46:05 +0200

    remove lambda middleware

--- src/routes/(visbot)/download/+server.ts
+++ src/routes/(visbot)/download/+server.ts
@@ -1,12 +1,24 @@
 import type { RequestHandler } from '@sveltejs/kit';
+import { promises as fs } from 'node:fs';
+import { resolve } from 'node:path';
 
-export const GET: RequestHandler = async () => {
-	const options: ResponseInit = {
-		status: 302,
+export const GET: RequestHandler = async (event) => {
+	const file = getFile(event.url.href);
+	const body = await fs.readFile(resolve(process.cwd(), 'static/files/packs', file));
+
+	return new Response(body, {
+		status: 200,
 		headers: {
-			location: '/.netlify/functions/download'
+			'Content-Type': 'application/zip',
+			'Content-Disposition': `filename="${file}`
 		}
-	};
-
-	return new Response(null, options);
+	});
 };
+
+function getFile(rawUrl: string): string {
+	const url = new URL(rawUrl);
+	const searchParams = new URLSearchParams(url.search);
+	const file = searchParams.get('file') || '';
+
+	return file;
+}
