1610a66263a0ca43854ec8cb766fcb050abb1004
Jan T. Sott <git@idleberg.com>
2024-12-15 21:11:46 +0100

    refactor bootstrap script

--- tools/bootstrap.js
+++ tools/bootstrap.js
@@ -1,18 +1,22 @@
 import shell from 'shelljs';
 
-shell.exec('npx degit wordpress/wordpress#6.0 .wordpress || echo "WordPress folder exists"');
+setupFakeWordpress();
 
-shell.mkdir('-p', 'static/wp-includes/css');
-shell.cp('.wordpress/wp-includes/css/buttons.min.css', 'static/wp-includes/css/buttons.min.css');
-shell.cp('.wordpress/wp-includes/css/dashicons.min.css', 'static/wp-includes/css/dashicons.min.css');
+function setupFakeWordpress() {
+	shell.exec('npx tiged wordpress/wordpress#6.0 .wordpress || echo "WordPress folder exists"');
 
-shell.mkdir('-p', 'static/wp-admin/css');
-shell.cp('.wordpress/wp-admin/css/forms.min.css', 'static/wp-admin/css/forms.min.css');
-shell.cp('.wordpress/wp-admin/css/install.css', 'static/wp-admin/css/login.min.css');
-shell.cp('.wordpress/wp-admin/css/l10n.min.css', 'static/wp-admin/css/l10n.min.css');
-shell.cp('.wordpress/wp-admin/css/login.min.css', 'static/wp-admin/css/login.min.css');
+	shell.mkdir('-p', 'static/wp-includes/css');
+	shell.cp('.wordpress/wp-includes/css/buttons.min.css', 'static/wp-includes/css/buttons.min.css');
+	shell.cp('.wordpress/wp-includes/css/dashicons.min.css', 'static/wp-includes/css/dashicons.min.css');
 
-shell.mkdir('-p', 'static/wp-admin/images');
-shell.cp('.wordpress/wp-admin/images/w-logo-blue.png', 'static/wp-admin/images/w-logo-blue.png');
-shell.cp('.wordpress/wp-admin/images/wordpress-logo.png', 'static/wp-admin/images/wordpress-logo.png');
-shell.cp('.wordpress/wp-admin/images/wordpress-logo.svg', 'static/wp-admin/images/wordpress-logo.svg');
+	shell.mkdir('-p', 'static/wp-admin/css');
+	shell.cp('.wordpress/wp-admin/css/forms.min.css', 'static/wp-admin/css/forms.min.css');
+	shell.cp('.wordpress/wp-admin/css/install.css', 'static/wp-admin/css/login.min.css');
+	shell.cp('.wordpress/wp-admin/css/l10n.min.css', 'static/wp-admin/css/l10n.min.css');
+	shell.cp('.wordpress/wp-admin/css/login.min.css', 'static/wp-admin/css/login.min.css');
+
+	shell.mkdir('-p', 'static/wp-admin/images');
+	shell.cp('.wordpress/wp-admin/images/w-logo-blue.png', 'static/wp-admin/images/w-logo-blue.png');
+	shell.cp('.wordpress/wp-admin/images/wordpress-logo.png', 'static/wp-admin/images/wordpress-logo.png');
+	shell.cp('.wordpress/wp-admin/images/wordpress-logo.svg', 'static/wp-admin/images/wordpress-logo.svg');
+}
