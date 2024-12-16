afdbaef1c8bdf5e615e10ea68946371cca8d6890
Jan T. Sott <git@idleberg.com>
2024-12-17 00:32:25 +0100

    refactor scan script

--- tools/scan.js
+++ tools/scan.js
@@ -1,27 +1,31 @@
-import { promises as fs } from 'fs';
-import path from 'path';
-import crypto from 'crypto';
+import { createHash } from 'node:crypto';
+import { promises as fs } from 'node:fs';
+import path from 'node:path';
 
 async function getFiles(dir) {
-	let files = await fs.readdir(dir, { withFileTypes: true });
-	let allFiles = [];
+	const files = await fs.readdir(dir, { withFileTypes: true });
 
-	for (let file of files) {
-		let fullPath = path.join(dir, file.name);
+	const filePromises = files.map(async (file) => {
+		const fullPath = path.join(dir, file.name);
+
 		if (file.isDirectory()) {
-			allFiles = allFiles.concat(await getFiles(fullPath));
+			return getFiles(fullPath);
 		} else if (file.isFile() && (file.name.endsWith('.7z') || file.name.endsWith('.zip'))) {
-			allFiles.push(fullPath);
+			return [fullPath];
+		} else {
+			return [];
 		}
-	}
+	});
 
-	return allFiles;
+	const nestedFiles = await Promise.all(filePromises);
+
+	return nestedFiles.flat();
 }
 
 async function getFileInfo(filePath, baseDir) {
 	const stats = await fs.stat(filePath);
 	const fileBuffer = await fs.readFile(filePath);
-	const hash = crypto.createHash('sha256').update(fileBuffer).digest('hex');
+	const hash = createHash('sha256').update(fileBuffer).digest('hex');
 	const relativePath = path.relative(baseDir, path.dirname(filePath));
 
 	return {
@@ -42,6 +46,7 @@ async function scanDirectory(directory) {
 
 // Get the input directory from command-line arguments
 const inputDir = process.argv[2];
+
 if (!inputDir) {
 	console.error('Please provide an input directory.');
 	process.exit(1);
